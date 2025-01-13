3e76f66eedf245847e952f9e4de09bff9454fd43
Ramon Ferreira <ramon7mf@gmail.com>
2025-01-13 13:29:22 -0300

    initial

---
+++ Seção 1 - Listas em JS/0 - Lista.mjs
@@ -0,0 +1,27 @@
+export const usuarios = [
+    {
+        nome: 'João',
+        idade: 23
+    },
+    {
+        nome: 'Gustavo',
+        idade: 42
+    },
+    {
+        nome: 'André',
+        idade: 17
+    },
+    {
+        nome: 'Jeremias',
+        idade: 64
+    },
+    {
+        nome: 'Heitor',
+        idade: 35
+    },
+    {
+        nome: 'Ethan',
+        idade: 28
+    },
+
+]
---
+++ Seção 1 - Listas em JS/Aula 01.mjs
@@ -0,0 +1,7 @@
+// Trabalhando com o array utilizando .map
+
+import { usuarios } from "./0 - Lista.mjs"
+
+console.log(usuarios.map((objeto) => {
+    return objeto.idade
+}))
